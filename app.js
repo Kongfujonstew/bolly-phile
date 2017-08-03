969c4f1e1cd77d16b4ea08c8ea51c02b559347d5
Kongfujonstew <jonstew@gmail.com>
2017-08-03 15:53:33 -0700

    npm init, scaffold &express/server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(express.static('src'));
+
+if (module === require.main) {
+  const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`server listening on port ${server.address().port}`);
+  });
+}
+
+module.exports = app;
