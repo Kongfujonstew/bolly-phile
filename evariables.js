b2028fce910f5bf38023e903e888c6cbb0553879
Kongfujonstew <jonstew@gmail.com>
2017-08-07 20:48:34 -0700

    update evariables for heroku2

--- evariables.js
+++ evariables.js
@@ -1,5 +1,5 @@
 module.exports = {
   domain: 'https://bolly-phile.herokuapp.com',
-  graphql: 'https://bolly-phile.herokuapp.com//graphql',
-  API_KEY: 'AIzaSyAxbx_3MCkSpTgCqH62eO2QXgAPuL4t4k'
+  graphql: 'https://bolly-phile.herokuapp.com/graphql',
+  API_KEY: 'AIzaSyAxbx_-3MCkSpTgCqH62eO2QXgAPuL4t4k'
 }
